5e761b4ab7914ff472149a304e489e6d6e35befe
Olayinka-Christianah <Olayinchristianah@gmail.com>
2024-06-21 05:49:30 +1300

    wrong-react

---
+++ src/Component/page1.jsx
@@ -0,0 +1,16 @@
+
+const page1 = () => {
+  return (
+    <div>
+        <div className="header">
+    
+    
+    <h3> A history in everything you copy</h3>
+    <h6>Clipboard allow you to track and organize Everything you copy. instantly  access your Clipboard on your
+      device </h6>
+    <button className="Green"> Download for iOS </button> <button className="blue"> Download for Mac </button>
+  </div></div>
+  )
+}
+
+export default page1
